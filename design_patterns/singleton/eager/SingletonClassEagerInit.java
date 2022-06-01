43c1c920e0ab8007e937f6df73a30435969ce7c2
dhirajab164 <dheerajab164@gmail.com>
2022-06-01 18:32:53 +0530

    Add Singleton eager initialization

---
+++ design_patterns/singleton/eager/SingletonClassEagerInit.java
@@ -0,0 +1,26 @@
+package com.dhiraj.design_patterns.singleton.eager;
+
+class SessionFactory {
+
+	private static final SessionFactory sessionFactory = new SessionFactory();
+
+	private SessionFactory() {
+	}
+
+	public static SessionFactory getInstance() {
+		return sessionFactory;
+	}
+}
+
+public class SingletonClassEagerInit {
+	public static void main(String[] args) {
+		SessionFactory sessionFactory = SessionFactory.getInstance();
+		SessionFactory sessionFactory1 = SessionFactory.getInstance();
+
+		System.out.println(sessionFactory);
+		System.out.println(sessionFactory1);
+
+		System.out.println(sessionFactory.equals(sessionFactory1));
+
+	}
+}
