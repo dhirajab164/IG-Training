7e6f9ca7cfa804321226ad6d0f6a5a9d759d2f13
dhirajab164 <dheerajab164@gmail.com>
2022-06-06 14:35:26 +0530

    Add Singleton class

---
+++ design_patterns/singleton/breaking/Singleton.java
@@ -0,0 +1,17 @@
+package com.dhiraj.design_patterns.singleton.breaking;
+
+public class Singleton {
+
+	private static Singleton singleton = null;
+
+	private Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		if (singleton == null) {
+			singleton = new Singleton();
+		}
+		return singleton;
+	}
+
+}
