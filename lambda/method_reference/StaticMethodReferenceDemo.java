91416bdacece4b03eb612632d951662360c8edac
dhirajab164 <dheerajab164@gmail.com>
2022-06-08 13:23:10 +0530

    Update StaticMethodReferenceDemo.java

--- lambda/method_reference/StaticMethodReferenceDemo.java
+++ lambda/method_reference/StaticMethodReferenceDemo.java
@@ -1,18 +1,13 @@
 package com.dhiraj.lambda.method_reference;
 
-@FunctionalInterface
-interface Drawable {
-	public void draw();
-}
-
 public class StaticMethodReferenceDemo {
 
+	// available static method implementation for draw method of Drawable functional interface
 	public static void drawSquare() {
 		System.out.println("Drawing Square.");
 	}
 
 	public static void main(String[] args) {
-
 		Drawable square = StaticMethodReferenceDemo::drawSquare;
 		square.draw();
 	}
