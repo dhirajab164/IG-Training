bc93185efb4699ef4f04422d35ac4ac4e3dec317
dhirajab164 <dheerajab164@gmail.com>
2022-06-12 22:32:19 +0530

    Update employee joined after 2015 for getting names

--- stream/list/employee/EmployeesJoinedAfter2015.java
+++ stream/list/employee/EmployeesJoinedAfter2015.java
@@ -1,14 +1,19 @@
 package com.dhiraj.stream.list.employee;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class EmployeesJoinedAfter2015 {
 	public static void main(String[] args) {
-		List<Employee> collect = Seeder.getEmployees().stream().filter(e -> e.getJoiningYear() > 2015)
+		System.out.println("Employees Joined after 2015");
+		List<Employee> employees = Seeder.getEmployees().stream().filter(e -> e.getJoiningYear() > 2015)
 				.collect(Collectors.toList());
-		collect.stream().forEach(System.out::println);
+		employees.stream().forEach(System.out::println);
+
+		System.out.println("Employee Names Joined after 2015");
+		List<String> employeeNames = Seeder.getEmployees().stream().filter(e -> e.getJoiningYear() > 2015)
+				.map(Employee::getName).collect(Collectors.toList());
+		employeeNames.stream().forEach(System.out::println);
 
 	}
 }
