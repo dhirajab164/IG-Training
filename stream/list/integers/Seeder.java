0ad7beea294e2431e37a357a521172bc2e200fa3
dhirajab164 <dheerajab164@gmail.com>
2022-06-15 20:03:31 +0530

    Add integer list seeder

---
+++ stream/list/integers/Seeder.java
@@ -0,0 +1,10 @@
+package com.dhiraj.stream.list.integers;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Seeder {
+	public static List<Integer> getIntegers() {
+		return Arrays.asList(10, 15, 8, 49, 25, 98, 15, 32, 98);
+	}
+}
