6e902d21c602762c5bed351b1881a267bc7ca794
dhirajab164 <dheerajab164@gmail.com>
2022-06-19 10:16:12 +0530

    Add ArrayListSeeder

---
+++ collection/list/array_list/traversing/ArrayListSeeder.java
@@ -0,0 +1,10 @@
+package com.dhiraj.collection.list.array_list.traversing;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListSeeder {
+	public static List<String> getList() {
+		return Arrays.asList("John", "Jane", "Jack", "James");
+	}
+}
