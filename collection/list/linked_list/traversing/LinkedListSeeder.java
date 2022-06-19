458d532e131d7a434a7e2351efe8087db53b68bf
dhirajab164 <dheerajab164@gmail.com>
2022-06-19 13:40:49 +0530

    Add LinkedListSeeder

---
+++ collection/list/linked_list/traversing/LinkedListSeeder.java
@@ -0,0 +1,10 @@
+package com.dhiraj.collection.list.linked_list.traversing;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class LinkedListSeeder {
+	public static LinkedList<String> getList() {
+		return new LinkedList<String>(Arrays.asList("John", "Jane", "Jack", "James"));
+	}
+}
