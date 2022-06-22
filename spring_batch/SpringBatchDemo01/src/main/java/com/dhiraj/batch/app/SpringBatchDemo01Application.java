551f960eeda0a79ccfee5cbcc4acb962f2e17da7
dhirajab164 <dheerajab164@gmail.com>
2022-06-22 17:11:03 +0530

    Add spring batch demo
    triggering job on rest endpoing to add data to h2 from file at classpath

---
+++ spring_batch/SpringBatchDemo01/src/main/java/com/dhiraj/batch/app/SpringBatchDemo01Application.java
@@ -0,0 +1,13 @@
+package com.dhiraj.batch.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchDemo01Application.class, args);
+	}
+
+}
