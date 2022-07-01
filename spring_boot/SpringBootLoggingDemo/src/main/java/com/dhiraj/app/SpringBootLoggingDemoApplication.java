4a2643ebed1ef7defe927fbb61d0dad8d91f5fea
dhirajab164 <dheerajab164@gmail.com>
2022-07-01 12:09:09 +0530

    Add logging into file config to application.properties

--- spring_boot/SpringBootLoggingDemo/src/main/java/com/dhiraj/app/SpringBootLoggingDemoApplication.java
+++ spring_boot/SpringBootLoggingDemo/src/main/java/com/dhiraj/app/SpringBootLoggingDemoApplication.java
@@ -1,12 +1,29 @@
 package com.dhiraj.app;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootLoggingDemoApplication {
 
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLoggingDemoApplication.class, args);
 	}
+
+	@PostConstruct
+	public void init() {
+		logger.info("init");
+		logger.trace("TRACE");
+		logger.info("INFO");
+		logger.debug("DEBUG");
+		logger.error("ERROR");
+		logger.warn("WARN");
+	}
+
 }
