4e5999850640a5f540bf3aa640b25c85975c2b68
dhirajab164 <dheerajab164@gmail.com>
2022-07-01 11:22:00 +0530

    Initial commit

---
+++ spring_boot/SpringBootLoggingDemo/src/test/java/com/dhiraj/app/SpringBootLoggingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhiraj.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
