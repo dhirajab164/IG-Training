af16c7d8f60b780f8d129da8198f1a587ecba2f3
dhirajab164 <dheerajab164@gmail.com>
2022-07-12 09:42:34 +0530

    Update LmsAppApplication

--- project/lms-app/src/main/java/com/dhiraj/app/LmsAppApplication.java
+++ project/lms-app/src/main/java/com/dhiraj/app/LmsAppApplication.java
@@ -1,66 +1,63 @@
 package com.dhiraj.app;
 
 import java.time.LocalDate;
-import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.dhiraj.app.entity.Book;
-import com.dhiraj.app.entity.Transaction;
 import com.dhiraj.app.entity.User;
 import com.dhiraj.app.entity.enums.Active;
-import com.dhiraj.app.entity.enums.TransactionStatus;
 import com.dhiraj.app.repository.BookRepository;
 import com.dhiraj.app.repository.TransactionRepository;
 import com.dhiraj.app.repository.UserRepository;
+import com.dhiraj.app.service.IBookService;
+import com.dhiraj.app.service.impl.BookServiceImpl;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class LmsAppApplication implements CommandLineRunner {
 
-	@Autowired
-	private UserRepository userRepository;
-	@Autowired
-	private BookRepository bookRepository;
-	@Autowired
-	private TransactionRepository transactionRepository;
-
-	Logger logger = LoggerFactory.getLogger(this.getClass());
+//	@Autowired
+//	private UserRepository userRepository;
+//
+//	@Autowired
+//	private IBookService bookService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LmsAppApplication.class, args);
-
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		log.info("Application started on." + LocalDate.now());
 		System.out.println("start");
 
-		List<User> users = userRepository.findAll();
-		users.forEach(System.out::println);
-
-		List<Book> books = bookRepository.findAll();
-		books.forEach(System.out::println);
-
-		User user1 = userRepository.findById(1L).get();
-		System.out.println(user1);
-
-		User lib1 = userRepository.findById(2L).get();
-		System.out.println(lib1);
-
-		Book book1 = bookRepository.findById(1L).get();
-		System.out.println(book1);
-
-		Transaction tr1 = new Transaction(book1, user1, lib1, Active.TRUE, TransactionStatus.PENDING, "NA",
-				LocalDate.now(), LocalDate.now().plusDays(3), user1, user1);
-		transactionRepository.save(tr1);
-
-		List<Transaction> trs = transactionRepository.findAll();
-		trs.forEach(System.out::println);
+//		User lib1 = userRepository.findById(2L).get();
+//		System.out.println(lib1);
+//
+//		Book book = Book.builder().title("Title").author("Author").publication("Publication").pages(120).price(150.0)
+//				.copies(100).active(Active.TRUE).createdBy(lib1).createdOn(LocalDate.now()).updatedBy(lib1)
+//				.updatedOn(LocalDate.now()).build();
+//
+//		// create
+//		Book createBook = bookService.createBook(book);
+//		System.out.println(createBook);
+//
+//		// update
+//		Book toUpdate = Book.builder().title("Title Updated").author("Author Updated")
+//				.publication("Publication Updated").pages(120).price(150.0).copies(100).active(Active.TRUE)
+//				.createdBy(lib1).createdOn(LocalDate.now()).updatedBy(lib1).updatedOn(LocalDate.now()).build();
+//		Book updateBook = bookService.updateBook(1, toUpdate);
+//		System.out.println("Updated Book: " + updateBook);
+//
+//		// delete
+//		bookService.deleteBookByid(1l);
+//		System.out.println("bookService.getBookById(1l): " + bookService.getBookById(1l));
 
 		System.out.println("end");
 	}
