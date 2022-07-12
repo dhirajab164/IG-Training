aac78837250be138d135187c87d774ce73afbfb5
dhirajab164 <dheerajab164@gmail.com>
2022-07-12 09:12:59 +0530

    update User add lombok support

--- project/lms-app/src/main/java/com/dhiraj/app/entity/User.java
+++ project/lms-app/src/main/java/com/dhiraj/app/entity/User.java
@@ -10,6 +10,15 @@ import com.dhiraj.app.entity.enums.Active;
 import com.dhiraj.app.entity.enums.Gender;
 import com.dhiraj.app.entity.enums.UserType;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 public class User {
 
@@ -36,125 +45,5 @@ public class User {
 	private UserType type;
 
 	private Active active = Active.TRUE;
-
-	public User() {
-		super();
-	}
-
-	public User(long id, String firstName, String lastName, int age, Gender gender, String email, String phone,
-			String city, UserType type, Active active) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.age = age;
-		this.gender = gender;
-		this.email = email;
-		this.phone = phone;
-		this.city = city;
-		this.type = type;
-		this.active = active;
-	}
-
-	public User(String firstName, String lastName, int age, Gender gender, String email, String phone, String city,
-			UserType type, Active active) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.age = age;
-		this.gender = gender;
-		this.email = email;
-		this.phone = phone;
-		this.city = city;
-		this.type = type;
-		this.active = active;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public Gender getGender() {
-		return gender;
-	}
-
-	public void setGender(Gender gender) {
-		this.gender = gender;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public UserType getType() {
-		return type;
-	}
-
-	public void setType(UserType type) {
-		this.type = type;
-	}
-
-	public Active getActive() {
-		return active;
-	}
-
-	public void setActive(Active active) {
-		this.active = active;
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", gender="
-				+ gender + ", email=" + email + ", phone=" + phone + ", city=" + city + ", type=" + type + ", active="
-				+ active + "]";
-	}
 
 }
