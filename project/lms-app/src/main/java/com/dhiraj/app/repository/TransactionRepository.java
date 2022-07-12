0a53821630276e42c3f40b1903c0078115575e7f
dhirajab164 <dheerajab164@gmail.com>
2022-07-12 14:10:29 +0530

    Update TransactionRepository.java

--- project/lms-app/src/main/java/com/dhiraj/app/repository/TransactionRepository.java
+++ project/lms-app/src/main/java/com/dhiraj/app/repository/TransactionRepository.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dhiraj.app.entity.Transaction;
+import com.dhiraj.app.entity.enums.TransactionStatus;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-	List<Transaction> findTransactionsByStatus(int status);
+	List<Transaction> findTransactionsByStatus(TransactionStatus status);
 
 }
