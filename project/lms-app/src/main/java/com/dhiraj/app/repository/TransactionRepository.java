47d0eb0866a591c67605e0ba3afeea5bc4e74496
dhirajab164 <dheerajab164@gmail.com>
2022-07-12 09:24:24 +0530

    Add Transaction repository

---
+++ project/lms-app/src/main/java/com/dhiraj/app/repository/TransactionRepository.java
@@ -0,0 +1,14 @@
+package com.dhiraj.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dhiraj.app.entity.Transaction;
+import com.dhiraj.app.entity.enums.TransactionStatus;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+	List<Transaction> findTransactionsByTransactionStatus(int status);
+
+}
