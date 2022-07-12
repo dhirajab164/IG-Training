0be8bc0bb80fdcdb296f49f18e5a6d46072b090a
dhirajab164 <dheerajab164@gmail.com>
2022-07-12 09:16:57 +0530

    Update Book service interface

--- project/lms-app/src/main/java/com/dhiraj/app/service/IBookService.java
+++ project/lms-app/src/main/java/com/dhiraj/app/service/IBookService.java
@@ -1,15 +1,12 @@
 package com.dhiraj.app.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import com.dhiraj.app.entity.Book;
-import com.dhiraj.app.entity.enums.Active;
-import com.dhiraj.app.entity.enums.TransactionStatus;
 
 public interface IBookService {
 
-	public Book createBook(Optional<Book> book);
+	public Book createBook(Book book);
 
 	public Book getBookById(long id);
 
@@ -23,7 +20,7 @@ public interface IBookService {
 
 	public List<Book> getAllBooksByPublication(String publication);
 
-	public Book updateBook(long id, Optional<Book> book);
+	public Book updateBook(long id, Book book);
 
 	public void deleteBookByid(long id);
 }
