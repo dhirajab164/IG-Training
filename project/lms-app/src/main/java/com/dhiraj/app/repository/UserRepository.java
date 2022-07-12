d32b5dbb2db47417014baec56f79cd7711e930e0
dhirajab164 <dheerajab164@gmail.com>
2022-07-12 14:10:34 +0530

    Update UserRepository.java

--- project/lms-app/src/main/java/com/dhiraj/app/repository/UserRepository.java
+++ project/lms-app/src/main/java/com/dhiraj/app/repository/UserRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dhiraj.app.entity.User;
+import com.dhiraj.app.entity.enums.UserType;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	List<User> findUsersByType(int type);
+	List<User> findUsersByType(UserType type);
 }
