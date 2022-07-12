8d59d465acd154afa6bab4d81ab06bc64c2f452e
dhirajab164 <dheerajab164@gmail.com>
2022-07-12 10:00:31 +0530

    Update global exception handler

--- project/lms-app/src/main/java/com/dhiraj/app/exception/GlobalExceptionHandler.java
+++ project/lms-app/src/main/java/com/dhiraj/app/exception/GlobalExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
-public class ControllerExceptionHandler {
+public class GlobalExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
 	@ResponseStatus(value = HttpStatus.NOT_FOUND)
