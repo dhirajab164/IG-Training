5188787d7e548b6199fe929ea0c42fbeb3d70161
dhirajab164 <dheerajab164@gmail.com>
2022-07-12 09:27:37 +0530

    Update BusinessException

--- project/lms-app/src/main/java/com/dhiraj/app/exception/BusinessException.java
+++ project/lms-app/src/main/java/com/dhiraj/app/exception/BusinessException.java
@@ -1,22 +1,31 @@
 package com.dhiraj.app.exception;
 
+import java.time.LocalDate;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class BusinessException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	private String message;
+	private int statusCode;
+	private LocalDate timestamp;
+	private String message, description;
 
 	public BusinessException(String message) {
 		super();
 		this.message = message;
 	}
 
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
+	public BusinessException(int statusCode, LocalDate timestamp, String message, String description) {
+		super();
+		this.statusCode = statusCode;
+		this.timestamp = timestamp;
 		this.message = message;
+		this.description = description;
 	}
 
 }
