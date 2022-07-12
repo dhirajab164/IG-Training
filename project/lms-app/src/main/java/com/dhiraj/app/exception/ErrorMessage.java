d55ad760609a3cdd60f78f202d51aa0615a3cd1e
dhirajab164 <dheerajab164@gmail.com>
2022-07-12 09:27:19 +0530

    Add ErrorMessage

---
+++ project/lms-app/src/main/java/com/dhiraj/app/exception/ErrorMessage.java
@@ -0,0 +1,55 @@
+package com.dhiraj.app.exception;
+
+import java.time.LocalDate;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ErrorMessage {
+	private int statusCode;
+	private LocalDate timestamp;
+	private String message, description;
+
+	public ErrorMessage(int statusCode, LocalDate timestamp, String message, String description) {
+		super();
+		this.statusCode = statusCode;
+		this.timestamp = timestamp;
+		this.message = message;
+		this.description = description;
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public LocalDate getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(LocalDate timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
