a38674f717d229be5d0b39a80658e4155795d24b
dhirajab164 <dheerajab164@gmail.com>
2022-07-14 15:37:38 +0530

    Add data.sql

---
+++ project/lms-app/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO `user` (`first_name`, `last_name`, `age`, `gender`, `email`, `phone`, `city`, `type`,`active`) VALUES("Olivero","Chipps",26,0,"ochipps0@myspace.com",6762553587,"Quisqueya",0,1);
+INSERT INTO `user` (`first_name`, `last_name`, `age`, `gender`, `email`, `phone`, `city`, `type`,`active`) VALUES("Ambur","Killigrew",21,1,"akilligrew1@yale.edu",3355576827,"Tonglin",1,1);
+INSERT INTO `user` (`first_name`, `last_name`, `age`, `gender`, `email`, `phone`, `city`, `type`,`active`) VALUES("Tally","Coram",21,0,"tcoram2@discuz.net",5921350089,"Gabi",1,1);
+INSERT INTO `user` (`first_name`, `last_name`, `age`, `gender`, `email`, `phone`, `city`, `type`,`active`) VALUES("Sydel","Vankov",25,1,"svankov3@un.org",3087386643,"Fenais da Ajuda",1,1);
+INSERT INTO `user` (`first_name`, `last_name`, `age`, `gender`, `email`, `phone`, `city`, `type`,`active`) VALUES("Persis","Bubear",21,1,"pbubear4@jugem.jp",6331640897,"Yukou",0,1);
+INSERT INTO `user` (`first_name`, `last_name`, `age`, `gender`, `email`, `phone`, `city`, `type`,`active`) VALUES("Ermengarde","Dumingos",29,1,"edumingos5@clickbank.net",8185657102,"Koltushi",0,1);
+INSERT INTO `user` (`first_name`, `last_name`, `age`, `gender`, `email`, `phone`, `city`, `type`,`active`) VALUES("Cart","Hagerty",21,0,"chagerty6@godaddy.com",6132146843,"Trinidad",0,1);
+INSERT INTO `user` (`first_name`, `last_name`, `age`, `gender`, `email`, `phone`, `city`, `type`,`active`) VALUES("Ignatius","Lapwood",28,0,"ilapwood7@infoseek.co.jp",9043086882,"San Bernardino",1,1);
+INSERT INTO `user` (`first_name`, `last_name`, `age`, `gender`, `email`, `phone`, `city`, `type`,`active`) VALUES("Noble","Caustick",26,0,"ncaustick8@twitpic.com",5386648389,"Bindang",0,1);
+INSERT INTO `user` (`first_name`, `last_name`, `age`, `gender`, `email`, `phone`, `city`, `type`,`active`) VALUES("Ramona","Gainor",28,1,"rgainor9@nhs.uk",4334370201,"La Coipa",1,1);
+INSERT INTO `book` (`title`, `author`, `publication`, `pages`, `price`, `copies`, `active`, `created_by`, `created_on`, `updated_by`, `updated_on`) VALUES('Title 1', 'Author 1', 'Publication 1', 120, 150, 50, 1, 2, now(), 2, now());
+INSERT INTO `book` (`title`, `author`, `publication`, `pages`, `price`, `copies`, `active`, `created_by`, `created_on`, `updated_by`, `updated_on`) VALUES('Title 2', 'Author 2', 'Publication 2', 120, 150, 50, 1, 2, now(), 2, now());
+INSERT INTO `book` (`title`, `author`, `publication`, `pages`, `price`, `copies`, `active`, `created_by`, `created_on`, `updated_by`, `updated_on`) VALUES('Title 3', 'Author 3', 'Publication 3', 120, 150, 50, 1, 2, now(), 2, now());
+INSERT INTO `book` (`title`, `author`, `publication`, `pages`, `price`, `copies`, `active`, `created_by`, `created_on`, `updated_by`, `updated_on`) VALUES('Title 4', 'Author 4', 'Publication 4', 120, 150, 50, 0, 2, now(), 2, now());
+INSERT INTO `book` (`title`, `author`, `publication`, `pages`, `price`, `copies`, `active`, `created_by`, `created_on`, `updated_by`, `updated_on`) VALUES('Title 5', 'Author 5', 'Publication 5', 120, 150, 50, 1, 2, now(), 2, now());
+INSERT INTO `book` (`title`, `author`, `publication`, `pages`, `price`, `copies`, `active`, `created_by`, `created_on`, `updated_by`, `updated_on`) VALUES('Title 6', 'Author 6', 'Publication 6', 120, 150, 50, 0, 2, now(), 2, now());
+INSERT INTO `book` (`title`, `author`, `publication`, `pages`, `price`, `copies`, `active`, `created_by`, `created_on`, `updated_by`, `updated_on`) VALUES('Title 7', 'Author 7', 'Publication 7', 120, 150, 50, 1, 2, now(), 2, now());
+INSERT INTO `book` (`title`, `author`, `publication`, `pages`, `price`, `copies`, `active`, `created_by`, `created_on`, `updated_by`, `updated_on`) VALUES('Title 8', 'Author 8', 'Publication 8', 120, 150, 50, 0, 2, now(), 2, now());
+INSERT INTO `transaction` (`active`, `created_on`, `issued_on`, `remark`, `status`, `to_be_returned_on`, `updated_on`, `book_id`, `created_by`, `issued_by`, `issued_to`, `updated_by`) VALUES ('1', '1', '2022-07-14', '2022-07-14', 'NA', '0', '2022-07-17', '2022-07-14', '1', '1', '2', '1', '1');
+
