698100029160ae66ad51fdc0148219685954ce9a
dhirajab164 <dheerajab164@gmail.com>
2022-07-14 15:37:58 +0530

    Update transaction service

--- project/lms-app/src/main/java/com/dhiraj/app/service/ITransactionService.java
+++ project/lms-app/src/main/java/com/dhiraj/app/service/ITransactionService.java
@@ -3,6 +3,7 @@ package com.dhiraj.app.service;
 import java.util.List;
 
 import com.dhiraj.app.entity.Transaction;
+import com.dhiraj.app.entity.enums.TransactionStatus;
 
 public interface ITransactionService {
 	public Transaction createTransaction(Transaction transaction);
@@ -11,7 +12,7 @@ public interface ITransactionService {
 
 	public List<Transaction> getAllTransactions();
 
-	public List<Transaction> getAllTransactionsByStatus(int status);
+	public List<Transaction> getAllTransactionsByStatus(TransactionStatus status);
 
 	public Transaction updateTransaction(long id, Transaction transaction);
 
