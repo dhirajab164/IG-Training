f723e4479e484e27ac3b0d048f58f19ddbce51b0
dhirajab164 <dheerajab164@gmail.com>
2022-07-16 17:33:01 +0530

    Update BookIssueStatus.java

--- project/lms-app/src/main/java/com/dhiraj/app/entity/enums/BookIssueStatus.java
+++ project/lms-app/src/main/java/com/dhiraj/app/entity/enums/BookIssueStatus.java
@@ -2,21 +2,21 @@ package com.dhiraj.app.entity.enums;
 
 import java.util.stream.Stream;
 
-public enum TransactionStatus {
+public enum BookIssueStatus {
 	PENDING(0), APPROVED(1), REJECTED(2);
 
 	private int status;
 
-	private TransactionStatus(int status) {
+	private BookIssueStatus(int status) {
 		this.status = status;
 	}
 
-	public int getTransactionStatus() {
+	public int getBookIssueStatus() {
 		return status;
 	}
 
-	public static TransactionStatus of(int status) {
-		return Stream.of(TransactionStatus.values()).filter(v -> v.getTransactionStatus() == status).findFirst()
+	public static BookIssueStatus of(int status) {
+		return Stream.of(BookIssueStatus.values()).filter(v -> v.getBookIssueStatus() == status).findFirst()
 				.orElseThrow(IllegalArgumentException::new);
 	}
 
