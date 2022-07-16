cfb4cbcb6fe0c9031ca029ca4c2ee33bd272e03b
dhirajab164 <dheerajab164@gmail.com>
2022-07-16 17:29:36 +0530

    Initial commit

---
+++ kafka/SpringBootKafkaStringMessageDemo/src/main/java/com/dhiraj/app/SpringBootKafkaStringMessageDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dhiraj.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaStringMessageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaStringMessageDemoApplication.class, args);
+	}
+
+}
