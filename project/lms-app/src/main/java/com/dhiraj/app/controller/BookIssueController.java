75df3c87fb62a00286520325fbfbe8b13cf04f68
dhirajab164 <dheerajab164@gmail.com>
2022-07-16 17:32:45 +0530

    Update BookIssueController.java

--- project/lms-app/src/main/java/com/dhiraj/app/controller/BookIssueController.java
+++ project/lms-app/src/main/java/com/dhiraj/app/controller/BookIssueController.java
@@ -1,6 +1,7 @@
 package com.dhiraj.app.controller;
 
 import java.time.LocalDate;
+
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -10,36 +11,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dhiraj.app.entity.Transaction;
-import com.dhiraj.app.entity.enums.TransactionStatus;
+import com.dhiraj.app.entity.BookIssue;
+import com.dhiraj.app.entity.enums.BookIssueStatus;
 import com.dhiraj.app.exception.ErrorMessage;
-import com.dhiraj.app.service.ITransactionService;
+import com.dhiraj.app.service.IBookIssueService;
 
 @RestController
-@RequestMapping("api/transactions")
-public class TransactionController {
-	private ITransactionService transactionService;
+@RequestMapping("api/book_issues")
+public class BookIssueController {
+	private IBookIssueService bookIssueService;
 
-	public TransactionController(ITransactionService transactionService) {
+	public BookIssueController(IBookIssueService bookIssueService) {
 		super();
-		this.transactionService = transactionService;
+		this.bookIssueService = bookIssueService;
 	}
 
 	@GetMapping
-	public ResponseEntity<List<Transaction>> getAllTransactions() {
-		List<Transaction> Transactions = transactionService.getAllTransactions();
-		return new ResponseEntity<List<Transaction>>(Transactions, HttpStatus.OK);
+	public ResponseEntity<List<BookIssue>> getAllBookIssues() {
+		List<BookIssue> BookIssues = bookIssueService.getAllBookIssues();
+		return new ResponseEntity<List<BookIssue>>(BookIssues, HttpStatus.OK);
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<?> getTransactionById(@PathVariable long id) {
+	public ResponseEntity<?> getBookIssueById(@PathVariable long id) {
 		if (id != 0) {
-			Transaction transaction = transactionService.getTransactionById(id);
-			return new ResponseEntity<Transaction>(transaction, HttpStatus.OK);
+			BookIssue bookIssue = bookIssueService.getBookIssueById(id);
+			return new ResponseEntity<BookIssue>(bookIssue, HttpStatus.OK);
 		} else {
 			ErrorMessage errorMessage = ErrorMessage.builder().statusCode(HttpStatus.BAD_REQUEST.value())
 					.message("Incorrect Id sent.").description("Id should be greater than zero").build();
@@ -48,11 +50,10 @@ public class TransactionController {
 	}
 
 	@GetMapping("/status/{status}")
-	public ResponseEntity<?> getAllTransactionsByAuthor(@PathVariable int status) {
+	public ResponseEntity<?> getAllBookIssuesByAuthor(@PathVariable int status) {
 		if (status >= 0 || status <= 2) {
-			List<Transaction> Transactions = transactionService
-					.getAllTransactionsByStatus(TransactionStatus.of(status));
-			return new ResponseEntity<List<Transaction>>(Transactions, HttpStatus.OK);
+			List<BookIssue> BookIssues = bookIssueService.getAllBookIssuesByStatus(BookIssueStatus.of(status));
+			return new ResponseEntity<List<BookIssue>>(BookIssues, HttpStatus.OK);
 		} else {
 			ErrorMessage errorMessage = ErrorMessage.builder().statusCode(HttpStatus.BAD_REQUEST.value())
 					.message("Incorrect Id sent.")
@@ -61,11 +62,11 @@ public class TransactionController {
 		}
 	}
 
-	@PostMapping("/transaction")
-	public ResponseEntity<?> createTransaction(@RequestBody Transaction transaction) {
-		if (transaction != null) {
-			Transaction createdTransaction = transactionService.createTransaction(transaction);
-			return new ResponseEntity<Transaction>(createdTransaction, HttpStatus.CREATED);
+	@PostMapping("/book_issue")
+	public ResponseEntity<?> createBookIssue(@RequestBody BookIssue bookIssue) {
+		if (bookIssue != null) {
+			BookIssue createdBookIssue = bookIssueService.createBookIssue(bookIssue);
+			return new ResponseEntity<BookIssue>(createdBookIssue, HttpStatus.CREATED);
 		} else {
 			ErrorMessage errorMessage = new ErrorMessage(HttpStatus.BAD_REQUEST.value(), LocalDate.now(),
 					"Book is Empty.", "Empty Object sent to save.");
@@ -73,34 +74,47 @@ public class TransactionController {
 		}
 	}
 
-	@PatchMapping("/transaction/{id}")
-	public ResponseEntity<?> updateTransaction(@PathVariable long id, @RequestBody Transaction transaction) {
-		if (id <= 0) {
-			ErrorMessage errorMessage = ErrorMessage.builder().statusCode(HttpStatus.BAD_REQUEST.value())
-					.message("Incorrect Id sent.").description("Id should be greater than zero").build();
-			return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
-		} else if (transactionService.getTransactionById(id) != null && transaction != null) {
-			Transaction updateTransaction = transactionService.updateTransaction(id, transaction);
-			return new ResponseEntity<Transaction>(updateTransaction, HttpStatus.ACCEPTED);
+	@PutMapping("/book_issue") // #lib/user/book
+	public ResponseEntity<?> updateBookIssue(@RequestBody BookIssue bookIssue) {
+		if (bookIssueService.getBookIssueById(bookIssue.getId()) != null && bookIssue != null) {
+			BookIssue updateBookIssue = bookIssueService.updateBookIssue(bookIssue);
+			return new ResponseEntity<BookIssue>(updateBookIssue, HttpStatus.ACCEPTED);
 		} else {
 			ErrorMessage errorMessage = new ErrorMessage(HttpStatus.BAD_REQUEST.value(), LocalDate.now(),
-					"Transaction is Empty.", "Empty Object sent to save.");
+					"BookIssue is Empty.", "Empty Object sent to save.");
 			return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
 		}
 	}
 
-	@DeleteMapping("/transaction/{id}")
-	public ResponseEntity<?> deleteTransactionById(@PathVariable long id) {
+	@PatchMapping("/book_issue/{id}/{user_id}")
+	public ResponseEntity<?> patchBookIssue(@PathVariable long id, @PathVariable long user_id,
+			@RequestBody BookIssue bookIssue) {
 		if (id <= 0) {
 			ErrorMessage errorMessage = ErrorMessage.builder().statusCode(HttpStatus.BAD_REQUEST.value())
 					.message("Incorrect Id sent.").description("Id should be greater than zero").build();
 			return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
-		} else if (transactionService.getTransactionById(id) != null) {
-			transactionService.deleteTransactionById(id);
-			return new ResponseEntity<String>("Transaction Deleted with id: " + id, HttpStatus.NO_CONTENT);
+		} else if (bookIssueService.getBookIssueById(id) != null && bookIssue != null) {
+			BookIssue updateBookIssue = bookIssueService.patchBookIssue(id, bookIssue);
+			return new ResponseEntity<BookIssue>(updateBookIssue, HttpStatus.ACCEPTED);
+		} else {
+			ErrorMessage errorMessage = new ErrorMessage(HttpStatus.BAD_REQUEST.value(), LocalDate.now(),
+					"BookIssue is Empty.", "Empty Object sent to save.");
+			return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@DeleteMapping("/book_issue/{id}")
+	public ResponseEntity<?> deleteBookIssueById(@PathVariable long id) {
+		if (id <= 0) {
+			ErrorMessage errorMessage = ErrorMessage.builder().statusCode(HttpStatus.BAD_REQUEST.value())
+					.message("Incorrect Id sent.").description("Id should be greater than zero").build();
+			return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
+		} else if (bookIssueService.getBookIssueById(id) != null) {
+			bookIssueService.deleteBookIssueById(id);
+			return new ResponseEntity<String>("BookIssue Deleted with id: " + id, HttpStatus.NO_CONTENT);
 		} else {
 			ErrorMessage errorMessage = ErrorMessage.builder().statusCode(HttpStatus.NOT_FOUND.value())
-					.message("Transaction not Found.").description("Transaction with ID: " + id + " not Available.")
+					.message("BookIssue not Found.").description("BookIssue with ID: " + id + " not Available.")
 					.build();
 			return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
 		}
