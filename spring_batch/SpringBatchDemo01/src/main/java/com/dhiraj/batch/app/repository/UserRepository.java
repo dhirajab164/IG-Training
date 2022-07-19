9f51407361c96d9c46c960bf533b2261b2baf8af
dhirajab164 <dheerajab164@gmail.com>
2022-07-19 11:05:21 +0530

    Update UserRepository.java

--- spring_batch/SpringBatchDemo01/src/main/java/com/dhiraj/batch/app/repository/UserRepository.java
+++ spring_batch/SpringBatchDemo01/src/main/java/com/dhiraj/batch/app/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package com.dhiraj.batch.app.repository;
+                                                                                                                                                                                                                                                                                             package com.dhiraj.batch.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
